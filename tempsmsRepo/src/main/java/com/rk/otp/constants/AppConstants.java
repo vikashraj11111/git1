c25604ade183d5ca271afcd0bfadac8829938be4
vikash <vikashraj11111@gmail.com>
2025-05-19 00:43:58 +0530

    Create AppConstants.java

---
+++ tempsmsRepo/src/main/java/com/rk/otp/constants/AppConstants.java
@@ -0,0 +1,20 @@
+package com.rk.otp.constants;
+
+public class AppConstants {
+	
+	private AppConstants() {
+		
+	}
+	
+	public static final String PAYMENT_METHOD_PAYTM = "PAYTM";
+	public static final String PAYMENT_METHOD_BHARATPE = "BHARATPE";
+	public static final String PAYMENT_METHOD_NOWPAYMENTS = "NOWPAYMENTS";
+	public static final String CURRENCY_INR = "INR";
+	public static final String STATUS_IN_PROGRESS = "I";
+	public static final String STATUS_FAILED = "F";
+	public static final String STATUS_SUCCESS = "S";
+	public static final String STATUS_CANCELLED = "C";
+	public static final String FALSE = "false";
+	public static final String FAILED = "FAILED";
+
+}
